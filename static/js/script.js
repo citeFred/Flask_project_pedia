9914a016c06e29d3f32d994530f378dfbe22dda9
INYONG KIM <ohnyong87@gmail.com>
2023-07-13 19:42:04 +0900

    Back2_Commit 2
    데이터 명세
    - DB : MongoDB
    - Collection : movie
    - Documents
    - 영화 URL : 'url':'url_receive' / from Frontend #url-> formData 'url_give':'value'
    - 별점 : 'star':'star_receive' / from Frontend #star-> formData 'star_give':'value'
    - 리뷰 내용 : 'comment':'comment_receive' / from Frontend formData #comment-> 'comment_give':'value'
    - 영화 제목 : 'title':ogtitle / from URL <meta[property="og:title"]['content']> 'value'
    - 영화 설명 : 'desc':ogdesc / from URL <meta[property="og:description"]['content']> 'value'
    - 영화 이미지 URL : 'image':ogimage / from URL <meta[property="og:image"]['content']> 'value'
    
    JavaScript의 posting()라는 함수 작성
    - index.html에서 값이 입력 된 input, select, textarea로부터 .val()을 통해 각 id에 해당되는 부분의 value를 얻어 url, comment, star에 변수에 저장
    - formData라는 객체를 생성
    - {'url_give':url}, { ... }, { ... } 라는 Dictionary 형식 데이터를 .append()를 통해  formData에 담기
    - posting()내부 fetch()를 통해 '/movie' URL에 대한 POST 방식 요청 (+ 위 formData 객체를 body로 추가하여 요청함)
    - .then()절로 app.py의 POST 부분의 jsonify 변환 객체가 반환된 이후
    - 반환 데이터는 첫번째 then()절의 res 인자값으로 들어감
    - 첫번째 then()으로 들어간 res는 *response.json()에 의해 Promise 객체로 변환
    - 해당 객체 데이터는 data라는 변수에 담기고
    - alert를 통해 key msg의 value인 'POST 연결 완료! + DB 저장 완료!'가 출력

--- static/js/script.js
+++ static/js/script.js
@@ -12,12 +12,25 @@ function listing() {
 
 // [Create]
 function posting() {
-    let formData = new FormData();
-    formData.append("sample_give", "샘플데이터");
+    // index.html로부터 값 가져오기
+    let url = $('#url').val()
+    let comment = $('#comment').val()
+    let star = $('#star').val()
 
+    // formData 객체를 생성하고
+    let formData = new FormData();
+    // formData.append("sample_give", "샘플데이터");
+    // append()통해 {key,value}를 객체에 담는다
+    formData.append("url_give", url);
+    formData.append("comment_give", comment);
+    formData.append("star_give", star);
+
+    // POST 요청에 위 formData를 body에 담아 요청한다.
     fetch('/movie', { method: "POST", body: formData }).then((res) => res.json()).then((data) => {
-        console.log(data)
+        // console.log(data)
         alert(data['msg'])
+        // 브라우저 새로고침 추가
+        window.location.reload()
     })
 }
 
